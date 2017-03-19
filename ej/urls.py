96ea3aaa359e10828bec67113ae91d13c6a50d7d
M.Dadm <mdadm.prog@gmail.com>
2017-03-19 17:13:26 +0500

    Added login_required

--- ej/urls.py
+++ ej/urls.py
@@ -1,4 +1,6 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.contrib.auth import views
 from . import views
 from .forms import PostForm
 
@@ -7,4 +9,8 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('ej.urls')),
 ]
--- ej/views.py
+++ ej/views.py
@@ -2,17 +2,21 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .forms import PostForm
 from .models import Post
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'ej/post_list.html', {'posts': posts})
 
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'ej/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -26,6 +30,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'ej/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -39,3 +44,9 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'ej/post_edit.html', {'form': form})
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
