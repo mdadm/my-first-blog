9ff1da1ee63059ceb3806f883d8276a6545e02b4
M.Dadm <mdadm.prog@gmail.com>
2017-03-20 17:25:50 +0500

    Добавлена авторизация пользователей, закрыт доступ незалогиненым пользователям

--- ej/urls.py
+++ ej/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views
+
 from . import views
 from .forms import PostForm
 
@@ -10,7 +11,7 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
-    url(r'^accounts/login/$', views.login, name='login'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('ej.urls')),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url('^', include('django.contrib.auth.urls'))
 ]
--- ej/views.py
+++ ej/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 from .forms import PostForm
 from .models import Post
-from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-@login_required
 def post_list(request):
+    if not request.user.is_authenticated():
+        return redirect('/login/?next=%s' % request.path)
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'ej/post_list.html', {'posts': posts})
 
